f1da63951566aecfc700f2f553f0a27c8fec4e2c
Antônio Neto <antonioizaiasgn@gmail.com>
2020-09-14 19:07:41 -0300

    Atualização do banco de dados

--- source/database.sql
+++ source/database.sql
@@ -5,5 +5,8 @@ CREATE TABLE IF NOT EXISTS usuario(
 	id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     login VARCHAR(255) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL
+    senha VARCHAR(12) NOT NULL
 );
+
+INSERT INTO usuario(nome, login, senha)
+VALUES('Administrator', 'admin', 'password');
